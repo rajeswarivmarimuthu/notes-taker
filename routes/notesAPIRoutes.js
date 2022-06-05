182ebd9f7385abd24a3b28bb9bbb05773cac164b
Raji Marimuthu <raji@modeanalytics.com>
2022-06-04 23:29:10 -0700

    committing code to send mails

--- routes/notesAPIRoutes.js
+++ routes/notesAPIRoutes.js
@@ -4,6 +4,10 @@ const notes = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 const uuid = require('../helper/uuid');
+const request = require('request');
+const axios = require('axios');
+const { compileFunction } = require('vm');
+require('dotenv').config();
 
 //Setting up the express middleware
 notes.use(express.json());
@@ -98,8 +102,67 @@ notes.get('/', (req, res) => {
       res.json('invalid request');
     };
   });
-  
 
+// notes.post('/createtemplate', async(req,res)=> {
+//   console.log('In createtemplate');
+//   templateMeta = {
+//     "name": "KnickKnack - New Request Notification",
+//     "title": "KnickKnack - New Request Notification",
+//     "html": "./public/email.html"
+//   }
+//   try {
+//       const resp = await axios.post(
+//       process.env.TRUSTIFI_URL + '/api/i/v1/template', 
+//       templateMeta,
+//       {headers: {
+//         'x-trustifi-key': process.env.TRUSTIFI_KEY,
+//         'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
+//         'Content-Type': 'application/json'
+//       }},
+//     );
+//     res.status(200).json(resp.data);
+//   } catch (err)
+//   {
+//     console.log (err);
+//     res.json('invalid request');
+//   }
+
+// })
+
+notes.post('/sendemail', async (req,res)=> {
+  console.log('In sendemail');
+  console.log ('req.body.requestor_notes ', req.body.requestor_notes);
+  console.log('req.body.requestor_email ', req.body.requestor_email);
+  console.log('req.body.provider_email ', req.body.provider_email);
+  console.log('req.body.provider_name ', req.body.provider_name);
+  const emailData = {
+    "template": {
+      "name": "KnickKnack - New Request Notification",
+      "fields": {
+        "second_field": req.body.requestor_notes,
+        "third_field": req.body.requestor_email
+      }
+    },
+    "recipients": [{"email": req.body.provider_email, "name": req.body.provider_name}]
+  };
+
+  try {
+    const resp = await axios.post(
+    process.env.TRUSTIFI_URL + '/api/i/v1/email', 
+    emailData,
+    {headers: {
+      'x-trustifi-key': process.env.TRUSTIFI_KEY,
+      'x-trustifi-secret': process.env.TRUSTIFI_SECRET,
+      'Content-Type': 'application/json'
+    }},
+  );
+  res.status(200).json(resp.data);
+}
+catch(err) {
+  console.log (err);
+  res.json('invalid request');
+}
+});
 
 module.exports = notes; 
 
